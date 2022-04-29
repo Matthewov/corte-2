4b54785475cb7292ef1b56f36fef495dfa9bd518
Matthewov <100739952+Matthewov@users.noreply.github.com>
2022-04-29 18:56:33 -0500

    test2

--- parcial.py
+++ parcial.py
@@ -1,1 +1,7 @@
-"profe no pude realizar el ejercicio."
+#parcial 2
+#array de numeros 
+
+array = ["8", "9", "10", "11"]
+array.reverse()
+print("El array incial es: ", arreglo)
+print("El array de salida es: ", arreglo)
